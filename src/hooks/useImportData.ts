7004406d86de5a951adfb2ffba83df17f1105aac
Mohamad-Hag <mohamadhag99@gmail.com>
2023-12-08 01:42:52 +0300

    fixing bugs

--- src/hooks/useImportData.ts
+++ src/hooks/useImportData.ts
@@ -5,11 +5,12 @@ import { replace as replacePreferences } from "redux/features/preferencesSlice";
 import { replace as replaceSidebar } from "redux/features/sidebarSlice";
 import { replace as replaceTodos } from "redux/features/todosSlice";
 import ImportExportData from "utils/interfaces/common/ExportData";
+import labels from "data/json/ui-labels.json";
 
 const useImportData = () => {
   let d = useDispatch();
 
-  return (importData: ImportExportData) => {
+  return (importData: ImportExportData): string => {
     let todoStore = importData.todoStore;
     let categoryStore = importData.categoryStore;
     let preferencesStore = importData.preferencesStore;
@@ -17,13 +18,17 @@ const useImportData = () => {
     let isImportDataValid =
       todoStore && categoryStore && preferencesStore && sidebarStore;
 
-    if (!isImportDataValid) return;
+    if (!isImportDataValid) {
+      return labels.cantImport;
+    }
 
     d(replaceTodos(todoStore));
     d(replaceCategories(categoryStore));
     d(replacePreferences(preferencesStore));
     d(replaceSidebar(sidebarStore));
     d(close());
+
+    return "";
   };
 };
 
